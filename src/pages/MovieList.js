b78a33995d5a37c46f0b00e91dfbcbf28cdc480b
Pranav S <pranavsvlm@gmail.com>
2023-09-09 01:59:12 +0530

    Updated Page Title

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,19 +1,21 @@
-import { Card } from "../components/Card"
-import { useFetch, useTitle } from "../hooks/index"
+import { useFetch } from "../hooks/useFetch";
+import { useTitle } from "../hooks/useTitle";
+import { Card } from "../components";
+
 
 export const MovieList = ({apiPath, title}) => {
-  const { data: movies } =useFetch(apiPath)
+  const { data: movies } = useFetch(apiPath);
   useTitle(title);
 
-    return (
-      <main>
-        <section className="max-w-7xl mx-auto py-7">
-          <div className="flex justify-start flex-wrap other:justify-evenly"> 
-            { movies.map((movie) => (
-              <Card key={movie.id} movie={movie} />
-            )) }     
-          </div>
-        </section>
-      </main>
-    )
-  }
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">       
+          { movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          )) }          
+        </div>
+      </section>
+    </main>
+  )
+}
