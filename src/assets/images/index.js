41094b4d8c172a44f8126fcbfba011264907a6f2
Pranav S <pranavsvlm@gmail.com>
2023-09-09 01:18:21 +0530

    Complete Files

---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+export { default as Instagram} from "./Instagram.svg"
+export { default as Linkedin} from "./LinkedIn.svg"
+export { default as Youtube} from "./YouTube.svg"
+export { default as GitHub} from "./GitHub.svg"
